a4dbb866356b314c2d12a602f38eab2e8b565392
YifanChen <moodygrey@qq.com>
2022-10-20 04:41:21 +0000

    fix include filesystem

--- parser-spef/parser-spef.hpp
+++ parser-spef/parser-spef.hpp
@@ -13,11 +13,13 @@
 #include <array>
 #include <string_view>
 #include <optional>
-#include <filesystem>
+#include <experimental/filesystem>
 #include <fstream>
 #include <cmath>
 
 #include "pegtl/pegtl.hpp"
+
+namespace fs = std::experimental::filesystem;
 
 namespace spef {
 
@@ -127,7 +129,7 @@ struct Spef {
   void scale_capacitance(float);
   void scale_resistance(float);
   
-  bool read(const std::filesystem::path &);
+  bool read(const fs::path &);
 
   template <typename T>
   friend struct Action;
@@ -1156,9 +1158,9 @@ const std::string Control<T>::error_message =
 
 
 // Procedure:: file_to_memory reads the content of a file to a string buffer
-inline std::string file_to_memory(const std::filesystem::path &p){
+inline std::string file_to_memory(const fs::path &p){
 
-  if(not std::filesystem::exists(p)){
+  if(not fs::exists(p)){
     return "";
   }
 
@@ -1174,7 +1176,7 @@ inline std::string file_to_memory(const std::filesystem::path &p){
 }
 
 // Function: read
-inline bool Spef::read(const std::filesystem::path &p){
+inline bool Spef::read(const fs::path &p){
 
   auto buffer {file_to_memory(p)};
 
